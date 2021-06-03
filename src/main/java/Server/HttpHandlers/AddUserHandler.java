03f605d12fd238989719652a8e4b8ea1b54295e9
KonradSuszko <34981439+KonradSuszko@users.noreply.github.com>
2021-06-03 21:14:13 +0200

    Auth

--- src/main/java/Server/HttpHandlers/AddUserHandler.java
+++ src/main/java/Server/HttpHandlers/AddUserHandler.java
@@ -4,7 +4,6 @@ import Server.DatabaseManager;
 import Server.Policy;
 import Server.Role;
 import Server.User;
-import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import lombok.AllArgsConstructor;
@@ -36,11 +35,27 @@ public class AddUserHandler implements HttpHandler {
             String password = json.getString("password");
             List<Role> roles = generateListOfRoles(json);
             List<Policy> policies = generateListOfPolicies(json);
-
-            User user = new User(username, password, roles, policies);
+            Role role = highestRole(json);
+            Policy policy = highestPolicy(json);
+            User user = new User(username, password, role, policy);
             System.out.println(user);
-            dm.add(user);
+            //User existent = dm.findByUsername(user.getUsername());
+            List<User> users = dm.findAll();
+            boolean exists = false;
+            for (User u : users){
+                if (u.getUsername().equals(user.getUsername())){
+                    exists = true;
+                    break;
+                }
+            }
             String response = "ok"; //ew obsluga tego ze jest juz taki uzytkownik
+            if (exists) {
+                response = "user already exists";
+            }
+            else{
+                dm.add(user);
+            }
+            //String response = "ok";
             exchange.sendResponseHeaders(200, response.getBytes(StandardCharsets.UTF_8).length);
             OutputStream os = exchange.getResponseBody();
             os.write(response.getBytes(StandardCharsets.UTF_8));
@@ -64,6 +79,28 @@ public class AddUserHandler implements HttpHandler {
             result.add(Role.USER);
         }
         return result;
+    }
+    private Role highestRole(JSONObject json) throws JSONException {
+        if(json.getBoolean("adminRole"))
+            return Role.ADMIN;
+        if(json.getBoolean("stuffRole"))
+            return Role.STUFF;
+        if(json.getBoolean("userRole"))
+            return Role.USER;
+        return null;
+    }
+    private Policy highestPolicy(JSONObject json) throws JSONException{
+        if(json.getBoolean("policy5"))
+            return Policy.AccessLevel5;
+        if(json.getBoolean("policy4"))
+            return Policy.AccessLevel4;
+        if(json.getBoolean("policy3"))
+            return Policy.AccessLevel3;
+        if(json.getBoolean("policy2"))
+            return Policy.AccessLevel2;
+        if(json.getBoolean("policy1"))
+            return Policy.AccessLevel1;
+        return null;
     }
     private List<Policy> generateListOfPolicies(JSONObject json) throws JSONException {
         List<Policy> result = new ArrayList<>();
